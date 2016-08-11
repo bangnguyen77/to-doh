a979e7ae6c23f4c3fa968033028df04bc401e076
Bang Nguyen <bangnv77@gmail.com>
2016-08-11 15:10:44 -0700

    list items turn blue if clicked, AppComponent can now listen to event

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,32 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
 @Component({
   selector: 'task-list',
   inputs: ['taskList'],
+  outputs: ['onTaskSelect'],
   template: `
-    <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">{{ currentTask.description }}</h3>
+  <h3 *ngFor="#currentTask of taskList"
+    (click)="taskClicked(currentTask)"
+    [class.selected]="currentTask === selectedTask">
+    {{ currentTask.description }}
+  </h3>
   `
 })
-
 export class TaskListComponent {
   public taskList: Task[];
+  public onTaskSelect: EventEmitter<Task>;
+  public selectedTask: Task;
+
+  constructor() {
+    this.onTaskSelect = new EventEmitter();
+  }
   taskClicked(clickedTask: Task): void {
-    console.log(clickedTask);
+    console.log('child', clickedTask);
+    this.selectedTask = clickedTask;
+    this.onTaskSelect.emit(clickedTask);
   }
 }
+
 
 @Component({
   selector: 'my-app',
@@ -21,23 +34,25 @@ export class TaskListComponent {
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <task-list [taskList]="tasks"></task-list>
+      <task-list
+        [taskList]="tasks"
+        (onTaskSelect)="taskWasSelected($event)">
+      </task-list>
     </div>
   `
 })
-
 export class AppComponent {
   public tasks: Task[];
-  constructor() {
+  constructor(){
     this.tasks = [
       new Task("Create To-Do List app.", 0),
       new Task("Learn Kung Fu.", 1),
       new Task("Rewatch all the Lord of the Rings movies.", 2),
-      new Task("Do the laundry.", 3),
+      new Task("Do the laundry.", 3)
     ];
   }
   taskWasSelected(clickedTask: Task): void {
-    console.log(clickedTask);
+    console.log('parent', clickedTask);
   }
 }
 
